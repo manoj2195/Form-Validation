b685d04a3fdb50ca448ee01ee2b2012b9e967cca
Manoj Nayak <monstermanoj7878@gmail.com>
2025-12-09 15:06:27 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,13 @@
 function validateForm() {
-  let name = document.getElementById("name").value; // .value = ye input field ka text return karta hai
+  let name = document.getElementById("name").value;
   let email = document.getElementById("email").value;
   let password = document.getElementById("password").value;
 
-  let regexp = new RegExp(/^\d/); // kisi pattern ko match karne ke liy ham regular expression ka use karte hai (\d = digit)
-  let regExEmail = new RegExp(/^[\w]+@[a-z]+\.[a-z]{2,}$/); // ( [] = ek ya ek se jayda word match karega ||) (w = word character) || (@+ a-z == character) || (.a-z = character)
-  // $ = string end // line end
+  let regexp = new RegExp(/^\d/); 
+  let regExEmail = new RegExp(/^[\w]+@[a-z]+\.[a-z]{2,}$/); 
   if (name === "") {
     alert("Name cannot be empty.");
-    return false; //yadi information wrong hai toh form submit nahi hoga
+    return false; 
   } else if (email === "") {
     alert("Email cannot be empty.");
     return false;
@@ -16,7 +15,7 @@ function validateForm() {
     alert("Password cannot be empty.");
     return false;
   } else if (regexp.test(name)) {
-    //yaha per ye syntax hai (jo ki name field pr laga hai)
+    
 
     alert("Name field cannot contain digit");
     return false;
@@ -28,3 +27,4 @@ function validateForm() {
     return false;
   }
 }
+
